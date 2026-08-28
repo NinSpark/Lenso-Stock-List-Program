c7209afd8ef222229063096eaa4f1efa0f9a11ff
NinSpark <tobylee3477@gmail.com>
2026-08-28 17:49:32 +0800

    change api url

--- frontend/src/services/stock.service.ts
+++ frontend/src/services/stock.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
     providedIn: 'root',
 })
 export class StockService {
-    private domain = "http://localhost:3000";
-    // private domain = "https://mcq5cp7n-3002.asse.devtunnels.ms";
+    // private domain = "http://localhost:3000";
+    private domain = "https://mcq5cp7n-3002.asse.devtunnels.ms";
     // private domain = "https://hbctrlpd-3000.asse.devtunnels.ms";
 
     private itemApiUrl = `${this.domain}/api/item`;
